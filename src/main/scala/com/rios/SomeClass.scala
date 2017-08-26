5fd351bdb25e6be52e6a0b670426d909a8988526
Gabriel Rios <gabrielfrios@gmail.com>
2017-08-26 15:02:53 -0300

    removing comments

--- src/main/scala/com/rios/SomeClass.scala
+++ src/main/scala/com/rios/SomeClass.scala
@@ -21,9 +21,4 @@ object Test extends App {
 
   someClass.print() // 2
   otherClass.print() // 1
-
-  // AWS-OpsWorks-Custom-Server,AWS-OpsWorks-Default-Server,databases,awseb-e-pbvg43kgcz-stack-AWSEBSecurityGroup-ZD2Z9I4548U0
-
-  // postgres://devs:99taxis2117@promotions-staging.cqela5ipg9g5.us-east-1.rds.amazonaws.com:5432/promotions
-  // postgres://devs:99taxis2117@promotions-staging.cqela5ipg9g5.us-east-1.rds.amazonaws.com:5432/promotions
 }
